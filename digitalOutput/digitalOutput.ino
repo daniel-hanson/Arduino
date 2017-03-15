d83d43f081aab396974b9b13fd791bbc20786300
Daniel Hanson <dhansonmu@gmail.com>
2017-03-15 14:28:44 -0500

    Added code for digital output.

---
+++ digitalOutput/digitalOutput.ino
@@ -0,0 +1,18 @@
+// Libraries
+#include <Wire.h>
+
+int outputToRelayPin = 8;
+
+void setup() {
+  // Setting the pin to output mode
+  pinMode(outputToRelayPin, OUTPUT);
+}
+
+uint8_t i=0;
+void loop() {
+  // Oscillating the output pin
+  digitalWrite(outputToRelayPin, HIGH);
+  delay(10000);
+  digitalWrite(outputToRelayPin, LOW);
+  delay(10000);
+}
