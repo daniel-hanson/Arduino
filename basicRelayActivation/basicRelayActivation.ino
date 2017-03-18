60ed6721ea9eb0d0df4dd81274131a7f7237e442
Daniel Hanson <dhansonmu@gmail.com>
2017-03-18 16:01:58 -0500

    Added simple relay activation and voltage montior

---
+++ basicRelayActivation/basicRelayActivation.ino
@@ -0,0 +1,15 @@
+// Libraries
+#include <Wire.h>
+
+int outputToRelayPin = 8;
+
+void setup() {
+  // Setting the pin to output mode
+  pinMode(outputToRelayPin, OUTPUT);
+}
+
+uint8_t i=0;
+void loop() {
+  // Oscillating the output pin
+  digitalWrite(outputToRelayPin, HIGH);
+}
---
+++ basicVoltageMonitor/basicVoltageMonitor.ino
@@ -0,0 +1,29 @@
+// Libraries
+// Changed with a new branch
+#include <Wire.h>
+
+int batVoltageAnalogPin = A1;
+
+void setup() {
+  // Setting the pin to output mode
+  Serial.begin(9600);
+}
+
+uint8_t i=0;
+void loop() {
+  // Basic Battery Voltage Monitor
+  int analogReadsPerAverageCalculation = 10;
+  long tempBatteryVoltage = 0;
+  long batteryVoltageSum = 0;
+  
+  for(int x = 0; x < analogReadsPerAverageCalculation; x++) {
+    long tempBatteryVoltage = 0;
+    tempBatteryVoltage = analogRead(batVoltageAnalogPin);
+    batteryVoltageSum += tempBatteryVoltage;
+    delay(10);
+  }
+
+  int averageBatteryVoltage = (int) (batteryVoltageSum / (long) analogReadsPerAverageCalculation);
+  Serial.print(averageBatteryVoltage);
+  Serial.println();
+}
